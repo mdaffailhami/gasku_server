783da4703eff85c8f70dc6a25e28e397195e4c89
Daffa Ilhami <mdaffailhami@gmail.com>
2024-06-29 06:12:27 +0800

    Allow All Origins (CORS)

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from routers import pages_router, pengguna_router, pangkalan_router, others_router
 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
