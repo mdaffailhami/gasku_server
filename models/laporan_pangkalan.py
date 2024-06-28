d71c055f87a06e94c3dc1cd728bd62cdba79e769
Daffa Ilhami <mdaffailhami@gmail.com>
2024-06-28 22:53:39 +0800

    Fix typo nim to nik

--- models/laporan_pangkalan.py
+++ models/laporan_pangkalan.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class LaporanPangkalan(BaseModel):
     jenis_laporan: str
-    nim_pelapor: str
+    nik_pelapor: str
     nama_pelapor: str
     id_pangkalan: str
     nama_pangkalan: str
